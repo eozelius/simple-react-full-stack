9367be8dff0d0a9b5d21c606dd4ee44d948bc1af
ethan ozelius <e.ozelius@gmail.com>
2018-11-07 10:49:56 -0500

    remove unnecessary files

--- src/client/components/LambdaHexagon.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import style from '../styles/LambdaHexagon.scss';
-
-const LambdaHexagon = props => <div className={style.hexagon} />;
-
-export default LambdaHexagon;
--- src/client/components/LambdaMap.js
+++ src/client/components/LambdaMap.js
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import style from '../styles/hexigon.scss';
-// import LambdaHexagon from './LambdaHexagon';
 
 export default class extends Component {
   constructor(props) {
